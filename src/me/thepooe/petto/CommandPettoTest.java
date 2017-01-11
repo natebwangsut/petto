37bcca8e579f37d2720411524c249ebcce215859
Bhurinat Wangsutthitham <bhurinat.wangsutthitham@thomsonreuters.com>
2017-01-11 17:31:01 +0700

    Add JUnit 5 Test Class

---
+++ src/me/thepooe/petto/CommandPettoTest.java
@@ -0,0 +1,15 @@
+package me.thepooe.petto;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by U6043820 on 1/11/2017.
+ */
+class CommandPettoTest {
+    @org.junit.jupiter.api.Test
+    void onCommand() {
+        assertTrue(true);
+    }
+}
