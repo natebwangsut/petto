948f0e65c9ce7a4aa174ca3e0ecb92bb37101814
Bhurinat Wangsutthitham <bhurinat.wangsutthitham@thomsonreuters.com>
2017-01-12 15:45:52 +0700

    Added Jacoco report

---
+++ src/me/thepooe/petto/test/CommandPettoTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by U6043820 on 1/12/2017.
+ */
+class CommandPettoTest {
+
+    @Test
+    @DisplayName("BlankTest")
+    void blankTest() {
+        System.out.println("blankTest");
+        assertEquals(2, 1+1);
+    }
+
+    @Test
+    void onCommandTest() {
+        System.out.println("onCommandTest");
+        assertTrue(true);
+    }
+
+}
